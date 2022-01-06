9f33cda15d1048cddbf2ac99293d8aaf5baa5755
istytsaniya <tsaniyaisty08@gmail.com>
2022-01-06 09:16:30 +0700

    first commit

---
+++ service.js
@@ -0,0 +1,35 @@
+function getAll(){
+    const respon = axios.get("http://localhost:8080/database/tsaniyastore")
+    const dr = respon.then(resp => resp.data)
+    return dr
+}
+
+async function create(data){
+    await axios.post("http://localhost:8080/database/tsaniyastore", data)
+    .then((result) => {
+        console.log(result)
+        return result.data
+    }).catch((err) => {
+        console.error(err)
+    });
+}
+    
+async function update(data){
+    await axios.put("http://localhost:8080/database/tsaniyastore", data)
+    .then((result) => {
+        console.log(result)
+        return result.data
+    }).catch((err) => {
+        console.error(err)
+    });
+}
+
+async function del(data){
+    await axios.delete("http://localhost:8080/database/tsaniyastore", data)
+    .then((result) => {
+        console.log(result)
+        return result.data
+    }).catch((err) => {
+        console.error(err)
+    });
+}
